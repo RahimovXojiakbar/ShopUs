b67f6800eb91572785243a1f4cfed23695df9496
RahimovXojiakbar <rahimovxojiakbar69@gmail.com>
2025-02-10 17:08:10 +0500

    add detail.html

--- main/urls.py
+++ main/urls.py
@@ -2,6 +2,7 @@ from . import views
 from django.urls import path    
 
 urlpatterns = [
-   path('', views.index_view),
+   path('', views.index_view, name='home'),
+   path('product-detail', views.detail_view, name='product-detil')
 ]
 
--- main/views.py
+++ main/views.py
@@ -3,3 +3,6 @@ from . import models
 
 def index_view(request):
     return render(request, 'home.html')
+
+def detail_view(request):
+    return render(request, 'product-detail.html')
