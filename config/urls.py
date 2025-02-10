aa3e2681958b8054511393c751b915c35e6a65d3
RahimovXojiakbar <rahimovxojiakbar69@gmail.com>
2025-02-10 15:40:02 +0500

    togrlandi

--- config/urls.py
+++ config/urls.py
@@ -20,11 +20,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.conf.urls.i18n import i18n_patterns
 
-urlpatterns = i18n_patterns(
+urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 
-)+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]+static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
  
 
--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,6 @@ from . import views
 from django.urls import path    
 
 urlpatterns = [
-   # path('', views.index_view),
-   path('', views.my_view ,name='lang')
+   path('', views.index_view),
 ]
 
